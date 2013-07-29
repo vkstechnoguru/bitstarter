c8a22f097ae286cd1292d307d2bddab8f2a1ff91
vkstechnoguru <vkstechnoguru@gmail.com>
2013-07-29 14:07:08 +0000

    adding index and reading from it

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buf = fs.readFileSync("index.html");
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
